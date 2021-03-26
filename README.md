73fb049c2edddeffc2ee411dfac413f4484d193b
Rowan Brumant <rowanb@comcast.net>
2021-03-26 15:12:42 -0500

    added resizing and color change capabilities

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 This is an etch-a-sketch program created to follow TheOdinProject's front-end web development curriculum.
 
 
+function playerPlay() {
+        playerPick = prompt("Please enter rock, paper, or scissors");
+        playerChoice = playerPick.toLowerCase().replace(/\s/g, "");
+        while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors"){
+          alert("Please try again and enter either rock, paper, or scissors");
+          playerPick = prompt("Please enter rock, paper, or scissors");
+          playerChoice = playerPick.toLowerCase().replace(/\s/g, "");
+        }
+        
+          return playerChoice;
+        }
--- script.js
+++ script.js
@@ -1,36 +1,84 @@
 const container = document.querySelector("#container");
+const cells = document.createElement("div");
+cells.classList.add("cells");
+const changeGrid = document.querySelector("#size");
+const eraser = document.querySelector("#erase");
+const randomColor = document.querySelector("#random");
+const blackColor = document.querySelector("#black");
+const clearGrid = document.querySelector("#clear");
+let numberOfSquares = 40;
 
-function randomize(number){
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var number = "rgb(" + x + "," + y + "," + z +")";
-    return(number);
+randomColor.addEventListener("click", function(){
+    const squares = document.querySelectorAll(".cells");
+    squares.forEach((cell) =>{
+        cell.addEventListener("mouseover", function(){
+            var x = Math.floor(Math.random() * 256);
+            var y = Math.floor(Math.random() * 256);
+            var z = Math.floor(Math.random() * 256);
+            color = "rgb(" + x + "," + y + "," + z +")";        
+            cell.style.backgroundColor = color;
+        })
+    });
+});
+
+blackColor.addEventListener("click", function(){
+    const squares = document.querySelectorAll(".cells");
+    squares.forEach((cell) =>{
+        cell.addEventListener("mouseover", function(){
+            cell.setAttribute("style", "background-color: black");
+        });
+    });
+});
+
+eraser.addEventListener("click", function(){
+    const squares = document.querySelectorAll(".cells");
+    squares.forEach((cell) =>{
+        cell.addEventListener("mouseover", function(){
+            cell.setAttribute("style", "background-color: white");
+        });
+    });
+});
+
+function blankGrid(){
+  var blank = document.getElementById("container");
+  var blank = removeGrid();
+  var blank = createGrid();
 }
 
-function randomColor(){
-    var getColor = document.getElementById("random");
-    getColor.style.color = randomize();
-}
+clearGrid.addEventListener("click", blankGrid);
 
-function blackFont(){
-    var changeFont = document.getElementById("random");
-    changeFont.style.color = "black";
+function removeGrid(){
+    container.innerHTML = "";
 }
 
 function createGrid(){
-    for (var i = 0; i < 55; i++){
-        var rows = document.createElement("div");
-        rows.className = "rows";
-        for (var j = 0; j < 110; j++){
-            var columns = document.createElement("div");
-            columns.className = "columns";
-            columns.addEventListener("mouseover", 
-            e => e.target.classList.add("fill-color"));
-            rows.appendChild(columns);
-        }
-        document.getElementById("container").appendChild(rows);
+    for (let i = 0; i < numberOfSquares ** 2; i++){
+        container.appendChild(cells.cloneNode());
     }
+        const squares = document.querySelectorAll(".cells");
+        squares.forEach((cell) =>{
+            cell.style.backgroundColor = "white";
+            cell.addEventListener("mouseover", function(){
+                cell.setAttribute("style", "background-color: black");
+            });
+        });
+    container.setAttribute('style', `grid-template: repeat(${numberOfSquares}, 
+        1fr) / repeat(${numberOfSquares}, 1fr);`);
 }
 
-container.appendChild(createGrid());
+changeGrid.addEventListener("click", function(){
+    var gridSize = prompt("Please enter a whole number between 1 and 100");
+    var num = Math.ceil(gridSize);
+    if (gridSize === null){
+        return;
+    }
+    if (num > 100 || num < 1 || Number.isInteger(num) === false){
+        alert("Error! You did not enter a whole number between 1 and 100");
+        return;
+        }
+    removeGrid();
+    numberOfSquares = gridSize;
+    createGrid();
+});
+
+createGrid();
